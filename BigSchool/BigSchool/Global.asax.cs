f1963f171fc5e196c465ea680b6548a8680871af
hoangphu1408 <hoangphu1408@gmail.com>
2020-06-03 16:12:09 +0700

    ??

--- BigSchool/BigSchool/Global.asax.cs
+++ BigSchool/BigSchool/Global.asax.cs
@@ -22,6 +22,19 @@ namespace BigSchool
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-       
+        public static class WebApiConfig
+        {
+            public static void Register(HttpConfiguration config)
+            {
+                // Web API routes
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            }
+        }
     }
 }
