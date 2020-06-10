766979a00886a7f1d757002391ca08dd129181bf
hoangphu1408 <hoangphu1408@gmail.com>
2020-06-10 15:59:48 +0700

    hoang

--- BigSchool/BigSchool/Models/ApplicationDbContext.cs
+++ BigSchool/BigSchool/Models/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
